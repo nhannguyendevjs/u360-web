e0716454199c138b2061e01542db5f2bf1a9f121
Nhan Nguyen <nhannguyen@appvity.com>
2024-04-25 23:08:30 +0700

    Clean up

--- u360/src/app/app.routes.ts
+++ u360/src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { authGuard } from './guards/auth.guard';
 
 export const routes: Routes = [
+  // Main routes
   {
     path: 'settings',
     loadChildren: () => import('./views/settings/settings.routes').then((m) => m.routes),
@@ -25,11 +26,15 @@ export const routes: Routes = [
     path: 'shell',
     loadChildren: () => import('./views/shell/shell.routes').then((m) => m.routes),
   },
+  // Children routes
+  // ...
+  // Default route
   {
     path: '',
     redirectTo: 'shell',
     pathMatch: 'full',
   },
+  // Not found route
   {
     path: '**',
     loadChildren: () => import('./views/not-found/not-found.routes').then((m) => m.routes),
