b1aa9a37b7b00fa67e2de64829d710d6c5e52b05
Nhan Nguyen <nhannguyen@appvity.com>
2024-05-04 09:26:51 +0700

    Clean up

--- u360/vitest.config.mjs
+++ u360/vitest.config.mjs
@@ -2,13 +2,15 @@ import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
   test: {
-    reporters: ['html'],
+    reporters: ['verbose', 'json', 'html'],
     outputFile: {
-      html: './dist/test-result/index.html',
+      html: './dist/test-result/html/index.html',
+      json: './dist/test-result/json/index.json',
     },
     coverage: {
+      provider: 'istanbul',
       reporter: ['html'],
-      reportsDirectory: './dist/test-coverage/unit/coverage',
+      reportsDirectory: './dist/test-result/coverage/html',
     },
   },
 });
