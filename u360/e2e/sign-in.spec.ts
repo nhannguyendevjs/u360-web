c314d4d42f7d2c79703495e97f637015954bb780
Nhan Nguyen <hoainhaannguyen@gmail.com>
2024-05-08 14:44:00 +0700

    Update e2e sign-in testing

--- u360/e2e/sign-in.spec.ts
+++ u360/e2e/sign-in.spec.ts
@@ -1,8 +1,15 @@
 import { test, expect } from '@playwright/test';
 
-test('has title', async ({ page }) => {
+test('Sign in successful', async ({ page }) => {
   await page.goto('localhost:4200/sign-in');
 
-  // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle(/Sign in/);
+  await expect(page.locator('#username')).toBeVisible();
+  await expect(page.locator('#password')).toBeVisible();
+
+  await page.fill('#username', 'admin');
+  await page.fill('#password', 'admin');
+  await page.click('#sign-in-button');
+  await page.waitForURL('**/home');
+  await expect(page).toHaveTitle(/Home/);
 });
