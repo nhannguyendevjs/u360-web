6b5be7fb2fc4e76957124127acf16aa1c3ff125c
Nhan Nguyen <nhannguyen@appvity.com>
2024-05-13 15:58:27 +0700

    Update routes config

--- u360/src/app/app.routes.ts
+++ u360/src/app/app.routes.ts
@@ -2,39 +2,63 @@ import { Routes } from '@angular/router';
 import { authGuard } from './guards/auth.guard';
 
 export const routes: Routes = [
-  // Main routes
-  {
-    path: 'settings',
-    loadChildren: () => import('./views/settings/settings.routes').then((m) => m.routes),
-    canActivate: [authGuard],
-  },
-  {
-    path: 'users',
-    loadChildren: () => import('./views/users/users.routes').then((m) => m.routes),
-    canActivate: [authGuard],
-  },
-  {
-    path: 'home',
-    loadChildren: () => import('./views/home/home.routes').then((m) => m.routes),
-    canActivate: [authGuard],
-  },
-  {
-    path: 'sign-in',
-    loadChildren: () => import('./views/sign-in/sign-in.routes').then((m) => m.routes),
-  },
-  {
-    path: 'shell',
-    loadChildren: () => import('./views/shell/shell.routes').then((m) => m.routes),
-  },
-  // Children routes
-  // ...
-  // Default route
+  //////////////////////////////////////////
+  // No Auth Routes
+  //////////////////////////////////////////
   {
     path: '',
-    redirectTo: 'shell',
-    pathMatch: 'full',
+    children: [
+      {
+        path: 'sign-in',
+        loadChildren: () => import('./views/sign-in/sign-in.routes').then((m) => m.routes),
+      },
+      {
+        path: 'shell',
+        loadChildren: () => import('./views/shell/shell.routes').then((m) => m.routes),
+      },
+      {
+        path: '',
+        redirectTo: 'shell',
+        pathMatch: 'full',
+      },
+    ],
   },
-  // Not found route
+  //////////////////////////////////////////
+  // Auth Routes
+  //////////////////////////////////////////
+  {
+    path: '',
+    canActivateChild: [authGuard],
+    children: [
+      {
+        path: 'settings',
+        loadChildren: () => import('./views/settings/settings.routes').then((m) => m.routes),
+        canActivate: [authGuard],
+      },
+      {
+        path: 'users',
+        loadChildren: () => import('./views/users/users.routes').then((m) => m.routes),
+        canActivate: [authGuard],
+      },
+      {
+        path: 'home',
+        loadChildren: () => import('./views/home/home.routes').then((m) => m.routes),
+        canActivate: [authGuard],
+      },
+    ],
+  },
+  //////////////////////////////////////////
+  // Child Auth Routes
+  //////////////////////////////////////////
+  // {
+  //   path: '',
+  //   canActivateChild: [authGuard],
+  //   children: [
+  //   ]
+  // },
+  //////////////////////////////////////////
+  // Not Found Route
+  //////////////////////////////////////////
   {
     path: '**',
     loadChildren: () => import('./views/not-found/not-found.routes').then((m) => m.routes),
